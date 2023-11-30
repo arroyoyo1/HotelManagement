b219287bf99310aaac4393a7de3616b0c12fa492
mauricio arroyo <arroyoyo@outlook.com>
2023-11-29 21:53:29 -0600

    Update Ticket.h

--- Ticket.h
+++ Ticket.h
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <vector>
 #include "Bebida.h"
+#include "Comida.h"
 #include "Cliente.h"
 #include "Producto.h"
 using namespace std;
@@ -14,22 +15,30 @@ class Ticket{
     private:
         string numeroFolio;
         vector<Producto> productos;
+        vector<Comida> comidas;
+        vector<Bebida> bebidas;
+
     public:
-    // Constructor por parametros
-    // El vector productos se inicializo como un vector vacio
-    // Agregacion
+    // Constructor por parámetros
+    // El vector productos se inicializó como un vector vacío
+    // Agregación
     Ticket(){}
-    Ticket (string numeroFolio_){
-           numeroFolio = numeroFolio_;
-    }
+    Ticket (string numeroFolio_);
     //Getter
     string getnumeroFolio();
     //Setter
     void setnumeroFolio(string);
     void agregarProducto(Producto);
+    void agregarBebida(Bebida);
+    void agregarComida(Comida);
     void imprimirProductos();
     float precioTicket();
 };
+
+Ticket::Ticket (string numeroFolio_){
+    numeroFolio = numeroFolio_;
+}
+
 string Ticket::getnumeroFolio(){
     return numeroFolio;
 }
@@ -40,15 +49,35 @@ void Ticket::setnumeroFolio(string numreroFolio_){
 void Ticket::agregarProducto(Producto nuevo_producto){
     productos.push_back(nuevo_producto);
 }
+
+void Ticket::agregarComida(Comida nueva_comida){
+    comidas.push_back(nueva_comida);
+}
+
+void Ticket::agregarBebida(Bebida nueva_bebida){
+    bebidas.push_back(nueva_bebida);
+}
+
+
 float Ticket::precioTicket(){
-    float precio_total = 0;
-    for (int i=0;i<productos.size();i++){
+    float precio_total = 0.0;
+    for(int i = 0; i < productos.size(); i++){
         precio_total += productos[i].precioFinal();
+
+    }
+    for(int i = 0; i < comidas.size(); i++){
+        precio_total += comidas[i].precioFinal();
+
+    }
+    for(int i = 0; i < bebidas.size(); i++){
+        precio_total += bebidas[i].precioFinal();
+
     }
     return precio_total;
 }
+
 void Ticket::imprimirProductos(){
-   for (int i=0;i<productos.size();i++){}
+   for (int i=0;i<productos.size();i++){i;}
 }
 
 
